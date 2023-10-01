cbffdf8586a5e77f93b4871b75dc6518770c1d4c
sushiiiii09 <sshreechoudhury@gmail.com>
2023-10-01 22:12:09 +0530

    feat: add two number

---
+++ Add_Two_number.cpp
@@ -0,0 +1,36 @@
+
+// Add Two Number
+//  You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummyHead = new ListNode(0);
+        ListNode *tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0)
+        {
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit2 = (l2 != nullptr) ? l2->val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode *newNode = new ListNode(digit);
+            tail->next = newNode;
+            tail = tail->next;
+
+            l1 = (l1 != nullptr) ? l1->next : nullptr;
+            l2 = (l2 != nullptr) ? l2->next : nullptr;
+        }
+
+        ListNode *result = dummyHead->next;
+        delete dummyHead;
+        return result;
+    }
+};
